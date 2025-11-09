4250f64d4b20a0f059b5670ce0f52984386aba99
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 19:53:07 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,60 @@
-// Interface for the printTeacher function
+// Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+// Director interface using SINGULAR as you want
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+// printTeacherFunction interface
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-// Function declaration
-function printTeacher(firstName: string, lastName: string): string {
+// printTeacher function
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
   return `${firstName[0]}. ${lastName}`;
 }
 
-// Example usage
-console.log(printTeacher("John", "Doe")); // Output: J. Doe
+// Example
+const director1: Director = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17
+};
+
+console.log(director1);
+
+// -------------------------------------------------------
+// STUDENT CLASS
+// -------------------------------------------------------
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
