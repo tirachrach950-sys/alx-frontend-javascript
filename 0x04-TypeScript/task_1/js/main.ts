d43189f2c192065c5b721c88ecea354978a32b82
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 19:08:07 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,59 +8,25 @@ interface Teacher {
   [key: string]: any;
 }
 
-
-interface Director extends Teacher {
-  numberOfReports: number;
-}
-
-// Function interface
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-// Function implementation
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}. ${lastName}`;
-};
-
-// Example object
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
-// Teacher interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
 // Director interface extending Teacher
 interface Director extends Teacher {
   numberOfReports: number;
 }
 
+// Interface for printTeacher function
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}. ${lastName}`;
-};
+// Function printTeacher EXACTLY as ALX requires
+function printTeacher(
+  { firstName, lastName }: { firstName: string; lastName: string }
+): string {
+  return `${firstName}. ${lastName}`;
+}
 
 // Example usage
-console.log(printTeacher("John", "Doe")); // J. Doe
-
-// Example Director
-const director1: Directors = {
+const director1: Director = {
   firstName: 'John',
   lastName: 'Doe',
   location: 'London',
@@ -69,3 +35,4 @@ const director1: Directors = {
 };
 
 console.log(director1);
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
