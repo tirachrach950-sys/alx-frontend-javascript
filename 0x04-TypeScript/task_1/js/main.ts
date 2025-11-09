fb51269f43e541e6048ef04c421240bbd7f1696a
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 18:55:04 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,35 +1,6 @@
-// Teacher interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
-// Directors interface extending Teacher
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-// Function interface for printTeacher
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-// printTeacher implementation
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}. ${lastName}`;
-};
-
-// Example object
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
+Object
+firstName: "John"
+fullTimeEmployee: true
+lastName: "Doe"
+location: "London"
+numberOfReports: 17
