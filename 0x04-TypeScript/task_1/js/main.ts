d8ebc306640d9689162148b121eec85d03a56006
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 20:00:22 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,60 +1,12 @@
-// Teacher interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
-// Director interface extending Teacher
-interface Director extends Teacher {
-  numberOfReports: number;
-}
-
-// Function interface
+// Interface for the printTeacher function
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
 // Function implementation
-const printTeacher: printTeacherFunction = function(firstName: string, lastName: string): string {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
-};
-
-// Example Director object
-const director1: Director = {
-  firstName: 'John',
-  lastName: 'Doe',
-  fullTimeEmployee: true,
-  location: 'London',
-  numberOfReports: 17
-};
-
-console.log(director1);
-console.log(printTeacher(director1.firstName, director1.lastName));
-
-// Class definition (strict checker wants this exact line)
-class StudentClass {
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return 'Currently working';
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
 }
 
 // Example usage
-const student1 = new StudentClass('Alice', 'Smith');
-console.log(student1.displayName());
-console.log(student1.workOnHomework());
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
