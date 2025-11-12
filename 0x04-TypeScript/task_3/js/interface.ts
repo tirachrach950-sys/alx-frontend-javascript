70fe4dbc72316bae265a12b7c7af0a98b2b2076f
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:06:41 +0100

    Update interface.ts

--- 0x04-TypeScript/task_3/js/interface.ts
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -9,4 +9,15 @@ export interface RowElement {
   lastName: string;
   age?: number;
 }
+// task_3/js/interface.ts
+
+// Type RowID
+export type RowID = number;
+
+// Interface RowElement
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
 
