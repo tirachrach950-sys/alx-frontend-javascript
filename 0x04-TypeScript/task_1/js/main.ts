131b9ede4075b644e01fb1189dcac6a55bae362d
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:35:22 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,37 @@
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+// task_1/js/main.ts
+
+// Interface for constructor arguments
+export interface StudentConstructor {
+  firstName: string;
+  lastName: string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName[0]}. ${lastName}`;
+// Interface for the class
+export interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
 }
 
+// Class implementation
+export class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student = new StudentClass({ firstName: "Guillaume", lastName: "Salva" });
+console.log(student.displayName());       // Output: Guillaume
+console.log(student.workOnHomework());    // Output: Currently working
