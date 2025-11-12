316f72a8941368d52a388fdcf7cd31f69c669b22
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 02:59:16 +0100

    Update main.ts

--- 0x04-TypeScript/ task_1/js/main.ts
+++ 0x04-TypeScript/ task_1/js/main.ts
@@ -1,4 +1,15 @@
-interface printTeacherFunction {
-  firstName, lastName
-  
+/ Define the interface for the object
+interface Person {
+  firstName: string;
+  lastName: string;
 }
+
+// Create a person object following the interface
+let person: Person = {
+  firstName: 'scripter',
+  lastName: 'efendi', // comma is required here
+};
+
+// Access properties
+console.log(person.firstName); // scripter
+console.log(person.lastName);  // efendi
