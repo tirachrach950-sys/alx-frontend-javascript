262169e380ddcf339c159579a905e5eb902a7784
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 04:44:16 +0100

    Update webpack.config.js

--- 0x04-TypeScript/task_2/webpack.config.js
+++ 0x04-TypeScript/task_2/webpack.config.js
@@ -1,1 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: "./js/main.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true }
+      }
+    ]
+  },
+  resolve: { extensions: [".tsx", ".ts", ".js"] },
+  devServer: { contentBase: "./dist" },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: "Task 2" })
+  ],
+  output: { filename: "bundle.js", path: path.resolve(__dirname, "dist") }
+};
 
