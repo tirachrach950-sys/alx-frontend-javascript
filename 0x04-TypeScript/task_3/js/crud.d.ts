efd5636d71bdb66e302484c347463207b91b576d
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:07:49 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -19,18 +19,3 @@ import { RowID, RowElement } from './interface';
 export declare function insertRow(row: RowElement): RowID;
 export declare function deleteRow(rowId: RowID): void;
 export declare function updateRow(rowId: RowID, row: RowElement): RowID;
-export function insertRow(row) {
-  console.log('Insert row', row);
-  return Math.floor(Math.random() * Math.floor(1000));
-}
-
-export function deleteRow(rowId) {
-  console.log('Delete row id', rowId);
-  return;
-}
-
-export function updateRow(rowId, row) {
-  console.log(`Update row ${rowId}`, row);
-  return rowId;
-}
-
