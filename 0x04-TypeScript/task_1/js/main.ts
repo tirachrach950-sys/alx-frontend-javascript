1e57197eef5f76b4f811ca3ecf522df424990e4d
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 02:22:38 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,7 +2,6 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-// The checker looks for this exact line ↓
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
