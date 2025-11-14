943fcd30301dc62ddaec3804bdc955d8814d9afb
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 19:15:40 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,37 +1,12 @@
-// task_1/js/main.ts
-
-// Interface for constructor arguments
-export interface StudentConstructor {
-  firstName: string;
-  lastName: string;
+// Interface for the function
+export interface printTeacherFunction {
+  (teacher: { firstName: string; lastName: string }): string;
 }
 
-// Interface for the class
-export interface StudentInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-// Class definition as ALX expects
-export class StudentClass {
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
+// Function implementation
+export const printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
 
 // Example usage
-const student = new StudentClass("Guillaume", "Salva");
-console.log(student.displayName());       // Guillaume
-console.log(student.workOnHomework());    // Currently working
+console.log(printTeacher({ firstName: "John", lastName: "Doe" })); // Output: J. Doe
